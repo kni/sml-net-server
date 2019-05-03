f501512dbcc9d93610167da4624731b9448f5e71
Nick Kostyria <kostirya@gmail.com>
2019-05-03 06:23:21 +0300

    printf

--- os-constants.c
+++ os-constants.c
@@ -4,11 +4,11 @@
 int main () {
 	printf("structure OS_Constants =\n");
 	printf("struct\n");
-	printf("  val SOL_SOCKET   = %zi\n", SOL_SOCKET);
+	printf("  val SOL_SOCKET   = %i\n", SOL_SOCKET);
 #if SO_REUSEPORT_LB
-	printf("  val SO_REUSEPORT = %zi\n", SO_REUSEPORT_LB);
+	printf("  val SO_REUSEPORT = %i\n", SO_REUSEPORT_LB);
 #else
-	printf("  val SO_REUSEPORT = %zi\n", SO_REUSEPORT);
+	printf("  val SO_REUSEPORT = %i\n", SO_REUSEPORT);
 #endif
 	printf("end\n");
 	return 0;
